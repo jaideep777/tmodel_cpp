b93e1cbcc0fcdddb44670e7277059b0ad020b2f5
Jaideep <jaideep777@gmail.com>
2024-06-07 18:52:25 +0200

    [feature/soil] add pf_splash app

---
+++ apps/pf_splash.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <numeric>
+#include <functional>
+#include <algorithm>
+
+#include "plantfate_patch.h"
+
+using namespace std;
+
+int main(int argc, char ** argv){
+
+	if (argc < 4){
+		cout << "Plant-FATE error: pf_splash needs three arguments. Syntax: ./pfate <params_file.ini> <start_year> <end_year>\n";
+		return 1;
+	}
+
+	string pfile = argv[1];
+	double y0 = stod(argv[2]), yf = stod(argv[3]);
+
+	if (y0 > yf){
+		cout << "Plant-FATE error: start year should be <= end year\n";
+	}
+
+	pfate::Patch sim(pfile);
+	sim.climate_stream.use_precip_data = true;
+	sim.init(y0, yf);
+	sim.spinup();
+	sim.simulate_coupled();
+	sim.close();
+
+	return 0;
+}
+
